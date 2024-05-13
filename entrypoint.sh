776e0e5e9543c4f0488df97e398b9eda5b410c0e
joel-mb <joel.moriana@gmail.com>
2024-05-13 15:06:19 +0200

    Add python version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-export PYTHONPATH=$PYTHONPATH:"${CARLA_ROOT}/PythonAPI/carla/dist/$(ls ${CARLA_ROOT}/PythonAPI/carla/dist | grep py3.)"
+PYTHON_VERSION=$(python3 -c "import sys; print('{}.{}'.format(sys.version_info[0], sys.version_info[1]))")
+export PYTHONPATH=$PYTHONPATH:"${CARLA_ROOT}/PythonAPI/carla/dist/$(ls ${CARLA_ROOT}/PythonAPI/carla/dist | grep py${PYTHON_VERSION})"
 export PYTHONPATH=$PYTHONPATH:"${CARLA_ROOT}/PythonAPI/carla"
 
 source "/opt/ros/$ROS_DISTRO/setup.bash"
